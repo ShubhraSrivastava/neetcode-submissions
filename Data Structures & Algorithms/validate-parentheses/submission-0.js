d0de5a05dc383c10c4e03334d0ed51fe37fca672
ShubhraSrivastava <shubhra.sjc13@gmail.com>
2026-09-15 23:39:33 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const brackets = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        };
+        for(const char of s) {
+            if(brackets[char]){
+                stack.push(brackets[char])
+                console.log("stack is: ",...stack)
+            }
+            else{
+                if(stack.length ===0 || stack.pop() !== char){
+                    return false;
+                }
+            }
+        }
+        return stack.length === 0
+    }
+}
